179ef86dc380f5c6a74c0b2c93c4a1591f75fae7
StrawIII <romisseiner@gmail.com>
2023-12-03 22:05:34 +0100

    small refactor

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from config import Config
 from interactive_bezier.interactive_bezier import App
 
 if __name__ == "__main__":
-    app = App()
-    app.run()
+    app = App(config=Config())
+    app.mainloop()
