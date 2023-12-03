441cfba69d90c27e24fea28c4f7a8d909ec67904
StrawIII <romisseiner@gmail.com>
2023-12-03 22:02:46 +0100

    add loading user settings from setting.toml

--- config.py
+++ config.py
@@ -1,6 +1,20 @@
-from pydantic_settings import BaseSettings
+#  DO NOT CHANGE ANYTHING BELOW UNLESS KNOW WHAT YOU ARE DOING!
+
 from pathlib import Path
+from typing import Tuple
+
+import tomli
+from pydantic import model_validator
+from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
     project_dir: Path = Path(__file__).parent
+    user_settings: dict
+
+    @model_validator(mode="before")
+    def load_user_settings(cls, values):
+        with Path(Path(__file__).parent, "settings.toml").open(mode="rb") as f:
+            values["user_settings"] = tomli.load(f)
+
+        return values
