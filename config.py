6c37de89336591763db31d0cb607c9248a5c0eaf
StrawIII <romisseiner@gmail.com>
2023-12-07 15:55:08 +0100

    add type checking for user settings

--- config.py
+++ config.py
@@ -1,16 +1,46 @@
 #  DO NOT CHANGE ANYTHING BELOW UNLESS KNOW WHAT YOU ARE DOING!
 
 from pathlib import Path
-from typing import Tuple
 
 import tomli
-from pydantic import model_validator
+from pydantic import ValidationError, model_validator
 from pydantic_settings import BaseSettings
+
+
+class PointSettings(BaseSettings):
+    diameter: int
+    color: str
+
+
+class LineSettings(BaseSettings):
+    thickness: int
+    color: str
+
+
+class UserSetting(BaseSettings):
+    fullscreen: bool
+    resolution: list  # tuple?
+    framerate: int
+    background_color: str
+    point: PointSettings
+    line: LineSettings
 
 
 class Config(BaseSettings):
     project_dir: Path = Path(__file__).parent
-    user_settings: dict
+    defaults: UserSetting = UserSetting(
+        fullscreen=True,
+        resolution=[1280, 720],
+        framerate=60,
+        background_color="gray30",
+        point=PointSettings(diameter=5, color="white"),
+        line=LineSettings(thickness=2, color="white"),
+    )
+    try:
+        user_settings: UserSetting
+    except ValidationError:
+        # TODO: finish defaut "user_settings" loading
+        user_settings: UserSetting = defaults
 
     @model_validator(mode="before")
     def load_user_settings(cls, values):
