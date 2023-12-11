9d448e21cd1c480116a5479a5f7a7b8c50ff5f84
StrawIII <romisseiner@gmail.com>
2023-12-11 16:31:01 +0100

    initial commit

---
+++ benchmarks.py
@@ -0,0 +1,43 @@
+from time import perf_counter
+
+import numpy as np
+
+from interactive_bezier.bezier import bezier_point, bezier_point_numpy
+from interactive_bezier.models import Layer, Point
+
+STEP_COUNT = 1000
+points_coor = [
+    (0, 0),
+    (1, 1),
+    (2, 1),
+    (3, 0),
+    (4, 7),
+]
+l1 = Layer(points=[Point(coor=coor) for coor in points_coor])
+l2 = np.array(object=points_coor, dtype=np.float32)
+
+
+def bezier():
+    start = perf_counter()
+
+    for step in range(0, STEP_COUNT, 1):
+        bezier_point(layer=l1, step=step / STEP_COUNT)
+
+    stop = perf_counter()
+    return f"bezier runtime:       {stop - start:.5f} s"
+
+
+def bezier_numpy():
+    start = perf_counter()
+
+    for step in np.arange(0, STEP_COUNT, 1):
+        bezier_point_numpy(layer=l2, step=step / STEP_COUNT)
+
+    stop = perf_counter()
+    return f"bezier numpy runtime: {stop - start:.5f} s"
+
+
+if __name__ == "__main__":
+    print(f"step count: {STEP_COUNT}")
+    print(bezier())
+    print(bezier_numpy())
