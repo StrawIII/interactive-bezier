8d2e9744aa5f6106a972816f3b96eda3066a538f
StrawIII <romisseiner@gmail.com>
2023-12-11 15:57:53 +0100

    remove try except

--- run.py
+++ run.py
@@ -2,9 +2,6 @@ from config import Config
 from interactive_bezier.app import App
 
 if __name__ == "__main__":
-    try:
-        app = App(config=Config())
-        app.setup()
-        app.mainloop()
-    except Exception as e:
-        print(f"Runtime error: {e}")
+    app = App(config=Config())
+    app.setup()
+    app.mainloop()
